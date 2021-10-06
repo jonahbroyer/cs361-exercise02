34c4ed3b266e797d531d5612efab389638a94438
Jonah Broyer <jkbsk8er@yahoo.com>
2021-10-05 18:38:47 -0700

    Removed comments and return statements.

--- main.rb
+++ main.rb
@@ -5,20 +5,16 @@ def modified_name(choose)
   reversed_name_components = split_name.reverse
   new_name1 = reversed_name_components.join(' ')
   @name = new_name1
-  puts new_name1
+
   if choose
-    # Split the name into last and first in an array
     split_name2 = new_name1.split
-    # Reverse the array of name components so the array is [last, first]
     reversed_name_components2 = split_name2.reverse
-    # Add borg to the end of the array
     reversed_name_components2 << "Borg"
-    # Rejoin the array of name components
     new_name2 = reversed_name_components2.join(' ')
-    puts new_name2
     @name = new_name2
   end
-  return @name
+
+  @name
 end
 
 puts "New name: #{modified_name(false)}"
