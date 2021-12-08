c621a1c5a3aa6b4edbb483c425301c9d7c3be51b
Jonah Broyer <jkbsk8er@yahoo.com>
2021-12-07 17:57:00 -0800

    Went through class walkthrough

--- main.rb
+++ main.rb
@@ -1,21 +1,12 @@
 @name = "Johanna Jackson"
 
-def modified_name(choose)
-  split_name = @name.split
-  reversed_name_components = split_name.reverse
-  new_name1 = reversed_name_components.join(' ')
-  @name = new_name1
-
-  if choose
-    split_name2 = new_name1.split
-    reversed_name_components2 = split_name2.reverse
-    reversed_name_components2 << "Borg"
-    new_name2 = reversed_name_components2.join(' ')
-    @name = new_name2
-  end
-
-  @name
+def reverse_name(name)
+  name.split.reverse.join(' ')
 end
 
-puts "New name: #{modified_name(false)}"
-puts "New borg name: #{modified_name(true)}"
+def borgify(name)
+  "#{name} Borg"
+end
+
+puts "New name: #{reverse_name(@name)}"
+puts "New borg name: #{borgify(@name)}"
